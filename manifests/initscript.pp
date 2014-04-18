72ca3bc3bdea1d5fa899d949ab3862a3ad36b483
Spencer Owen <sowen@adaptivecomputing.com>
2014-04-18 15:30:45 -0600

    Adds ubuntu to vagrant Changes intit and log file to dashes (redis_6390 => redis-6390)

--- manifests/initscript.pp
+++ manifests/initscript.pp
@@ -2,7 +2,7 @@ class initscript() {
 
   notice("Applying ${fpm_osfamily} init script")
 
-  file {"${::fpm_installdir}/etc/init.d/redis_${::fpm_redis_port}":
+  file {"${::fpm_installdir}/etc/init.d/redis-${::fpm_redis_port}":
       ensure => file,
     # content => "${fpm_redis_pwd}/templates/redis_init_${::osfamily}.erb",
     content => template("${fpm_redis_pwd}/templates/redis_init_${::fpm_osfamily}.erb"),
