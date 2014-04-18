b9915756653e5dee10170afb80da267184c4f58a
Spencer Owen <owenspencer@gmail.com>
2014-04-17 23:17:58 -0600

    Adds template

--- manifests/startupscript.pp
+++ manifests/startupscript.pp
@@ -1,7 +1,8 @@
 class startupscript() {
 
-  file {"${fpm_installdir}/etc/init.d/redis_${::fpm_redis_port}":
-    source => "${fpm_redis_pwd}/templates/redis_init_${::osfamily}.erb",
+  file {"${::fpm_installdir}/etc/init.d/redis_${::fpm_redis_port}":
+    # content => "${fpm_redis_pwd}/templates/redis_init_${::osfamily}.erb",
+    content => template("${fpm_redis_pwd}/templates/redis_init_${::osfamily}.erb"),
   }
     
 }
--- redis-rhel.sh
+++ redis-rhel.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
+set -x 
 PATH=$PATH:/bin:/usr/bin:/usr/local/bin
 
-$FPM_REDIS_PORT="6390"
-$FPM_REDIS_VERSION="2.8.8"
-$FPM_EMAIL='sowen@adaptivecomputing.com'
-$FPM_ITTERATION="1"
 
-$FPM_INSTALLDIR='/tmp/installdir'
-$FPM_REDIS_URL="http://download.redis.io/releases/"
-$FPM_NAME="redis-$FPM_REDIS_PORT"
+export FACTER_FPM_REDIS_PORT="6390"
+export FACTER_FPM_REDIS_VERSION="2.8.8"
+export FACTER_FPM_EMAIL='sowen@adaptivecomputing.com'
+export FACTER_FPM_ITTERATION="1"
+
+export FACTER_FPM_REDIS_PWD=`pwd`
+export FACTER_FPM_INSTALLDIR='/tmp/installdir'
+export FACTER_FPM_REDIS_URL="http://download.redis.io/releases"
+# export FACTER_FPM_NAME="redis-$FACTER_FPM_REDIS_PORT"
+
+yum install -y puppet
 
 # Install prerequisits for fpm
-yum install gcc rpm-build
-yum install ruby-devel   #alternativly use rbenv or RVM to install ruby development libaries
-yum install rubygems
-gem install fpm
-#fpm --help
+puppet module install puppetlabs/stdlib --modulepath=/tmp/modules
+
+puppet apply ./manifests/packages.pp --modulepath=/tmp/modules
+
 
 # Download and extract redis
-mkdir -p $FPM_INSTALLDIR
-cd /tmp
-wget $FPM_REDIS_URL/redis-$FPM_REDIS_VERSION.tar.gz -O /tmp/redis-$FPM_REDIS_VERSION.tar.gz
-tar -xvf /tmp/redis-$FPM_REDIS_VERSION.tar.gz
-cd /tmp/redis-$FPM_REDIS_VERSION
+mkdir -p $FACTER_FPM_INSTALLDIR
+wget $FACTER_FPM_REDIS_URL/redis-$FACTER_FPM_REDIS_VERSION.tar.gz -O /tmp/redis-$FACTER_FPM_REDIS_VERSION.tar.gz
+tar -xvf /tmp/redis-$FACTER_FPM_REDIS_VERSION.tar.gz -C /tmp
+#cd /tmp/redis-$FACTER_FPM_REDIS_VERSION
 
 # Compile and install redis
-PREFIX=$FPM_INSTALLDIR/usr/local/ make 
-PREFIX=$FPM_INSTALLDIR/usr/local/ make install 
-mkdir -p $FPM_INSTALLDIR/etc/init.d/
-mkdir -p $FPM_INSTALLDIR/etc/redis/
-mkdir -p $FPM_INSTALLDIR/var/lib/redis/$FPM_REDIS_PORT
+make hiredis jemalloc linenoise lua -C /tmp/redis-$FACTER_FPM_REDIS_VERSION/deps
+PREFIX=$FACTER_FPM_INSTALLDIR/usr/local/ make -C /tmp/redis-$FACTER_FPM_REDIS_VERSION
+PREFIX=$FACTER_FPM_INSTALLDIR/usr/local/ make install -C /tmp/redis-$FACTER_FPM_REDIS_VERSION
+mkdir -p $FACTER_FPM_INSTALLDIR/etc/init.d/
+mkdir -p $FACTER_FPM_INSTALLDIR/etc/redis/
+mkdir -p $FACTER_FPM_INSTALLDIR/var/lib/redis/$FACTER_FPM_REDIS_PORT
+
+# Retreive init script from online 
+#wget https://gist.githubusercontent.com/spuder/9401395/raw -O /tmp/redis_$FACTER_FPM_REDIS_VERSION.erb
+puppet apply ./manifests/startupscript.pp --modulepath=/tmp/modules
 
